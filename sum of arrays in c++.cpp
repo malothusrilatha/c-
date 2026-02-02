560d59b982c8343dfe1b1754a4eab048717a4de8
malothusrilatha <malothusrilatha443@gmail.com>
2026-02-02 21:24:57 +0530

    Add files via upload

---
+++ sum of arrays in c++.cpp
@@ -0,0 +1,15 @@
+ #include <iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cout << "Enter n: ";
+    cin >> n;
+    for (int i = 2; i <= n; i += 2) {
+        sum += i;
+    }
+    cout << "Sum of alternate integers = " << sum;
+    return 0;
+}
+
+
+
